a32bdd079ca864dbbdb1353f16ab08af1ad6e8f6
kaiwei-zheng <3087644479@qq.com>
2026-09-19 17:07:14 +0800

    Add files via upload

--- code/gradient_descent_for_linner_regression/gradient_descent_for_linner_regression.py
+++ code/gradient_descent_for_linner_regression/gradient_descent_for_linner_regression.py
@@ -23,7 +23,41 @@ class GradientDescent:
         b -= self.lr * db
         return w, b
 
- 
+
+class OptLinearRegression:
+    def __init__(self, lr=0.01, epochs=5000):
+        self.lr = lr
+        self.epochs = epochs
+        self.w = None
+        self.b = None
+        self.loss_function = MSELoss()
+        self.opt = GradientDescent(lr=self.lr)
+
+    def optimal(self, X, y):
+        n, d = X.shape
+        print(f"sample: n={n}, attribute: d={d}")
+
+        # Feature Standardization
+        X = (X - X.mean(axis=0)) / X.std(axis=0)
+
+        self.w = np.zeros(d)
+        self.b = 0.0
+
+        for i in range(self.epochs):
+            y_pred = X @ self.w + self.b
+            loss = self.loss_function.compute_loss(y_pred, y)
+            dw, db = self.loss_function.compute_grad(X, y_pred, y)
+            self.w, self.b = self.opt.step(self.w, self.b, dw, db)
+
+            if i % 50 == 0:
+                print(f"iteration{i:3d} | loss={loss:.4f}")
+
+        final_loss = self.loss_function.compute_loss(X @ self.w + self.b, y)
+        print(f"finish: loss={final_loss:.4f}, b={self.b:.4f}")
+
+    def predict(self, X):
+        return X @ self.w + self.b
+
 if __name__ == "__main__":
     # Parse 128 column names
     names = []
@@ -47,29 +81,5 @@ if __name__ == "__main__":
     feature_names = df.columns.tolist()
     X = df.to_numpy()
 
-    n, d = X.shape
-    print(f"sample: n={n}, attribute: d={d}")
-
-    # Feature Standardization
-    X = (X - X.mean(axis=0)) / X.std(axis=0)
-
-    w = np.zeros(d)
-    b = 0.0
-    loss_function = MSELoss()
-    opt = GradientDescent(lr=0.01)
-
-    for i in range(5000):
-        y_pred = X @ w + b
-        loss = loss_function.compute_loss(y_pred, y)
-        dw, db = loss_function.compute_grad(X, y_pred, y)
-        w, b = opt.step(w, b, dw, db)
-
-        if i % 50 == 0:
-            print(f"iteration{i:3d} | loss={loss:.4f}")
-
-    final_loss = loss_function.compute_loss(X @ w + b, y)
-    print(f"finish: loss={final_loss:.4f}, b={b:.4f}")
-
-    print("w:")
-    for name, w in zip(feature_names, w):
-        print(f"  {name:24s} = {w:+.4f}")
+    model = OptLinearRegression(lr=0.01, epochs=5000)
+    model.optimal(X, y)
